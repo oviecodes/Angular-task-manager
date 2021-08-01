b8d8eb7a7cb9200b5c3429a452fdb43876e99344
Godwin Alexander <alecgee73@gmail.com>
2021-08-01 16:06:40 +0100

    ccreate todo-list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodoListComponent } from './todo-list/todo-list.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    ReactiveFormsModule,
+    AppRoutingModule,
+    RouterModule.forRoot([
+      { path: '', component: TodoListComponent },
+    ])
+  ],
+  declarations: [
+    AppComponent,
+    TodoListComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/todo-list/todo-list.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-todo-list',
+  templateUrl: './todo-list.component.html',
+  styleUrls: ['./todo-list.component.scss']
+})
+export class TodoListComponent implements OnInit {
+
+  allTodos = ['learn TypeScript', 'learn Angular']
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
